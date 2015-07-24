37c04201736b413f9d9dd318fe2bab5e2f95a42c
王旭光 <wxgb9801@hotmail.com>
2015-07-24 15:01:46 +0800

    1

--- Measurer/UI/Testbed.Designer.cs
+++ Measurer/UI/Testbed.Designer.cs
@@ -151,7 +151,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(677, 72);
+            this.button1.Location = new System.Drawing.Point(680, 110);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(131, 34);
